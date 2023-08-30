4de23bb67fc2b0753dc2a17e9109b2437be3657f
GitHub Action <action@github.com>
2023-08-30 09:34:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/wangdingfu/ideacodingbuddy/services/MyProjectService.kt
+++ src/main/kotlin/com/github/wangdingfu/ideacodingbuddy/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.wangdingfu.ideacodingbuddy.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wangdingfu.ideacodingbuddy.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
